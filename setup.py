bfa6efa4ff35184de0c68285d34a539750680d01
Jason Moore <moorepants@gmail.com>
2012-03-16 17:15:27 -0700

    Moved the dtk package inside the yeadon package. Maybe this is a fix for the namespace issues we've found when also trying to import DynamicistsToolKit.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Chris Dembia',
     author_email='chris530d@gmail.com',
     version='0.9',
-    packages=['yeadon','dtk'],
+    packages=['yeadon','yeadon.dtk'],
     url="http://pypi.python.org/pypi/yeadon",
     license='LICENSE.txt',
     long_description=open('README.rst').read(),
