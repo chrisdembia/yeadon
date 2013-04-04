5b8cb25c615b70903d2657be6057f878da740cc3
Jason Moore <moorepants@gmail.com>
2013-04-04 19:51:19 -0400

    inverse --> tranpose

--- yeadon/tests/test_inertia.py
+++ yeadon/tests/test_inertia.py
@@ -33,7 +33,7 @@ def test_euler_rotation(display=False):
     R = inertia.euler_rotation(a, order)
 
     # definition of body 1-2-3 rotations from Spacecraft Dynamics, Kane,
-    # Likins, Levinson, 1982 page 423 (this is the inverse of what is
+    # Likins, Levinson, 1982 page 423 (this is the tranpose of what is
     # presented)
     C = mat([[c2 * c3, s1 * s2 * c3 + s3 * c1, -c1 * s2 * c3 + s3 * s1],
              [-c2 * s3, -s1 * s2 * s3 + c3 * c1, c1 * s2 * s3 + c3 *s1],
@@ -61,7 +61,7 @@ def test_euler_rotation(display=False):
     R = inertia.euler_rotation(a, order)
 
     # definition of body 3-1-3 rotations from Spacecraft Dynamics, Kane,
-    # Likins, Levinson, 1982 page 424 (this is the inverse of what is
+    # Likins, Levinson, 1982 page 424 (this is the tranpose of what is
     # presented)
     C = mat([[-s1 * c2 * s3 + c3 * c1, c1 * c2 * s3 + c3 *s1, s2 *s3],
              [-s1 * c2 * c3 - s3 * c1, c1 * c2 * c3 - s3 * s1, s2 * c3],
@@ -88,7 +88,7 @@ def test_euler_rotation(display=False):
     R = inertia.euler_rotation(a, order)
 
     # definition of body-three 1-3-2 rotations from Spacecraft Dynamics, Kane,
-    # Likins, Levinson, 1982 page 423 (this is the inverse of what is
+    # Likins, Levinson, 1982 page 423 (this is the tranpose of what is
     # presented)
 
     C = mat([[c2 * c3, c1 * s2 * c3 + s3 * s1, s1 * s2 * c3 - s3 * c1],
@@ -117,7 +117,7 @@ def test_euler_rotation(display=False):
     R = inertia.euler_rotation(a, order)
 
     # definition of body 2-1-3 rotations from Spacecraft Dynamics, Kane,
-    # Likins, Levinson, 1982 page 423 (this is the inverse of what is
+    # Likins, Levinson, 1982 page 423 (this is the tranpose of what is
     # presented)
 
     C = mat([[s1 * s2 * s3 + c3 * c1, c2 * s3, c1 * s2 * s3 - c3 * s1],
