31053bbe7bf8af7a6dbc86b421477161a64012b1
Christopher Dembia <cld72@cornell.edu>
2013-06-25 00:50:38 -0700

    Update version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yeadon',
-    version='1.0.0',
+    version='1.0.1',
     author='Chris Dembia',
     author_email='chris530d@gmail.com',
     url="https://github.com/fitze/yeadon/",
