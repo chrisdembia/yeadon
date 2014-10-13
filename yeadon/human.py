090ad8275181d253f3b309f0f7d2834d02fb2651
Jason K. Moore <moorepants@gmail.com>
2014-10-13 17:02:35 -0400

    Removed the annoying print statement if Mayavi doesn't import.

--- yeadon/human.py
+++ yeadon/human.py
@@ -13,9 +13,8 @@ import numpy as np
 import yaml
 try:
     from mayavi import mlab
-except Exception as e:
-    print "Failed to import mayavi. This is fine, it just means that you " \
-          "cannot use the draw() member function."
+except ImportError:
+    pass
 
 import inertia
 import solid as sol
@@ -25,6 +24,7 @@ from exceptions import YeadonDeprecationWarning
 
 # Display our warnings to the user.
 warnings.simplefilter('always', YeadonDeprecationWarning)
+
 
 class Human(object):
     measnames = ('Ls1L', 'Ls2L', 'Ls3L', 'Ls4L', 'Ls5L', 'Ls6L', 'Ls7L',
