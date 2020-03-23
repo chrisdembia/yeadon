f2c3d3c15e56022e931c25a676f08777298bd622
Jason K. Moore <moorepants@gmail.com>
2020-03-23 13:55:40 -0700

    Removed Py 3.3 classifier.

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
