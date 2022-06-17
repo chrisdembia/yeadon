e76fb3e3f277aca80fde7786be24075ae1e15a10
Francisco Pascoa <francisco.pascoa@umontreal.ca>
2022-06-17 11:46:47 -0400

    fixed utils.printoptions
    FloatFormat was marked as deprecated in numpy 1.14.0 and its deprecation expired in numpy 1.18.0.
    
    Furthermore, FloatingFormat.__call__ takes only 2 positional arguments so it would crash when passing strip_zeros.
    
    After a quick grep in the files, turns out that nor printoptions(True, **kwargs) nor printoptions(strip_zeros=True) was used anywhere, therefore the keyword was removed.
    
    This now works with numpy 1.22.4.

--- yeadon/utils.py
+++ yeadon/utils.py
@@ -7,7 +7,7 @@ import numpy.core.arrayprint as arrayprint
 
 
 @contextlib.contextmanager
-def printoptions(strip_zeros=False, **kwargs):
+def printoptions(**kwargs):
     """Allows you to set NumPy array print formatting locally.
 
     Taken from:
@@ -16,14 +16,7 @@ def printoptions(strip_zeros=False, **kwargs):
 
     """
 
-    origcall = arrayprint.FloatFormat.__call__
-
-    def __call__(self, x, strip_zeros=strip_zeros):
-        return origcall.__call__(self, x, strip_zeros)
-
-    arrayprint.FloatFormat.__call__ = __call__
     original = np.get_printoptions()
     np.set_printoptions(**kwargs)
     yield
     np.set_printoptions(**original)
-    arrayprint.FloatFormat.__call__ = origcall
